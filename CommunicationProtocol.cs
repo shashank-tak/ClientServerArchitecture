a712654437a40ed5c3135afcbf965beece68f885
Shashank tak <72602491+shashank-tak@users.noreply.github.com>
2023-12-08 15:46:55 +0530

    Add files via upload

---
+++ CommunicationProtocol.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NetworkProtocol
+{
+    public class CommunicationProtocol
+    {
+        public int size;
+        public string REQUEST = "request";
+        public string RESPONSE = "response";
+        public byte[] data;
+        public string protocolVersion;
+        public string protocolFormat;
+        public string protocolType;
+        public string sourceIp;
+        public int sourcePort;
+        public string destIp;
+        public int destPort;
+        public Hashtable headers;
+
+        public void setHeaders(Hashtable headers)
+        {
+            this.headers = headers;
+        }
+
+        public Hashtable getHeaders()
+        {
+            return headers;
+        }
+    }
+}
