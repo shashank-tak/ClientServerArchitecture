cd3a07a5ed7228214ff85b1bb3ebfadd79d5f881
Shashank tak <72602491+shashank-tak@users.noreply.github.com>
2023-12-08 15:45:23 +0530

    Add files via upload

---
+++ ITTSportsCarnivalTests/DataConverterTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITTSportsCarnivalApp.DataConverter;
+using ITTSportsCarnivalApp.Models;
+using System.IO;
+using System.Text.RegularExpressions;
+using ITTSportsCarnivalApp;
+
+namespace ITTSportsCarnivalTests
+{
+    [TestClass]
+    public class DataConverterTests
+    {
+        DataConverterClass _dataConverter = new DataConverterClass();
+        [TestMethod]
+        public void DeserializeJsonIntoObjects_Success()
+        {
+            //Arrange
+            string inputFilePath = "D://Team1.json";
+
+            //Act
+            var result = _dataConverter.DeserializeJsonIntoObjects<GameModel>(inputFilePath);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(GameModel));
+        }
+
+        [TestMethod]
+        public void DeserializeJsonIntoObjects_Failure()
+        {
+            //Arrange
+            string inputFilePath = "D://Team1.json";
+
+            //Act
+            var result = _dataConverter.DeserializeJsonIntoObjects<string>(inputFilePath);
+
+            //Assert
+            Assert.AreEqual(result,ApplicationConstants.DeserializeJsonIntoObjectsFailureMessage);
+        }
+
+        [TestMethod]
+        public void SerializeJsonIntoObjects_Success()
+        {
+            //Arrange
+            var filePath = "D://Serializer.json";
+            string inputFilePath = "D://Team1.json";
+            GameModel game = _dataConverter.DeserializeJsonIntoObjects<GameModel>(inputFilePath);
+
+            //Act
+            var result = _dataConverter.SerializeJsonIntoObjects(filePath, game);
+
+            //Assert
+            var actual = Regex.Replace(File.ReadAllText(result), @"\s+", " "); 
+            var expected = Regex.Replace(File.ReadAllText(inputFilePath), @"\s+", " ");
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void SerializeJsonIntoObjects_Failure()
+        {
+            //Arrange
+            var filePath = "";
+            string inputFilePath = "D://Team1.json";
+            var game = "wrong object";
+
+            //Act
+            var result = _dataConverter.SerializeJsonIntoObjects(filePath, game);
+
+            //Assert
+            Assert.AreEqual(result, ApplicationConstants.SerializeJsonIntoObjectsFailureMessage);
+        }
+    }
+}
---
+++ ITTSportsCarnivalTests/EventManagerTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ITTSportsCarnivalApp.Models;
+using ITTSportsCarnivalApp.DataConverter;
+using ITTSportsCarnivalApp.Controllers;
+using System.Collections.Generic;
+
+namespace ITTSportsCarnivalTests
+{
+    [TestClass]
+    public class EventManagerTests
+    {
+        DataBaseContext _dbContext = new DataBaseContext();
+        EventManagerController eventManagerController = new EventManagerController();
+
+        [TestMethod]
+        public void CreateTeam_WithValidData()
+        {
+            //Arrange
+            var dataConverter = new DataConverterClass();
+            string outputFilePath = "D://Team2Output.json";
+            GameModel game = dataConverter.DeserializeJsonIntoObjects<GameModel>("D://Team1.json");
+
+            //Act
+            var result = eventManagerController.CreateTeamForEvents(game, outputFilePath);
+
+            //Assert
+            Assert.IsTrue(result > 0);
+
+        }
+
+        [TestMethod]
+        public void CreateTeam_WithInValidPlayers()
+        {
+            //Arrange
+            var dataConverter = new DataConverterClass();
+            string outputFilePath = "D://Team2Output.json";
+            GameModel game = dataConverter.DeserializeJsonIntoObjects<GameModel>("D://Team3.json");
+
+            //Act
+            var result = eventManagerController.CreateTeamForEvents(game, outputFilePath);
+
+            //Assert
+            Assert.IsTrue(result == 0);
+
+        }
+
+        [TestMethod]
+        public void CreateTeam_WithInValidData()
+        {
+            //Arrange
+            var dataConverter = new DataConverterClass();
+            string outputFilePath = "D://TeamOutput.json";
+            GameModel game = dataConverter.DeserializeJsonIntoObjects<GameModel>("D://Team2.json");
+
+            //Act
+            var result = eventManagerController.CreateTeamForEvents(game, outputFilePath);
+
+            //Assert
+            Assert.IsTrue(result == 0);
+
+        }
+
+        [TestMethod]
+        public void CreateTeam_WithInValidFilePath()
+        {
+            //Arrange
+            var dataConverter = new DataConverterClass();
+            string outputFilePath = "";
+            GameModel game = dataConverter.DeserializeJsonIntoObjects<GameModel>("D://Team2.json");
+
+            //Act
+            var result = eventManagerController.CreateTeamForEvents(game, outputFilePath);
+
+            //Assert
+            Assert.IsTrue(result == 0);
+
+        }
+
+        [TestMethod]
+        public void GetTeam_WithValidGameType()
+        {
+            //Arrange
+            var gameTypeId = 1;
+            string outputFilePath = "D://Team1Output.json";
+
+            //Act
+            var result = eventManagerController.GetTeams(gameTypeId, outputFilePath);
+
+            //Assert
+            Assert.IsTrue(result >= 0);
+        }
+
+        [TestMethod]
+        public void GetTeam_WithInValidGameType()
+        {
+            //Arrange
+            var gameTypeId = 10;
+            string outputFilePath = "D://Team1Output.json";
+
+            //Act
+            var result = eventManagerController.GetTeams(gameTypeId, outputFilePath);
+
+            //Assert
+            Assert.IsTrue(result < 0);
+        }
+    }
+}
